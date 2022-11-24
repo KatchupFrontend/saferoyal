b0fdc74843d220941eb7b0741d3fb7d6091cfa68
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-24 21:10:04 +0000

    spacing

--- booking-project/src/components/HomeHero.jsx
+++ booking-project/src/components/HomeHero.jsx
@@ -7,7 +7,7 @@ const HomeHero = () => {
         Book affordable rooms with
         <span className="hover:text-blue-500"> SafeRoyal</span>
       </h1>
-      <p className="text-xl ">
+      <p className="text-xl px-4">
         Find the best rooms at the best prices with our help.
       </p>
       <button className="m-2 bg-blue-500 rounded-lg px-10 py-2 hover:bg-blue-600">
