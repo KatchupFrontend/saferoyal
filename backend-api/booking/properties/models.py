6577e2e5c8ce3fac8d269d7dfc1fc26b7828a621
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-25 22:09:22 +0000

    emailfied added

--- backend-api/booking/properties/models.py
+++ backend-api/booking/properties/models.py
@@ -9,7 +9,7 @@ class Customer(models.Model):
     user  = models.OneToOneField(User, on_delete=models.CASCADE)
     name  = models.CharField(max_length=200, null=True)
     phone = models.PositiveBigIntegerField()
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     def __str__(self):
         return self.name
