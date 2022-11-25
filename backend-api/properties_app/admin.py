d33f7cb6eff23cb100c16ca04db645cd93f604ef
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-25 23:27:03 +0000

    models scaled

---
+++ backend-api/properties_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import CampusCategory, Room
+# Register your models here.
+admin.site.register(Room)
+admin.site.register(CampusCategory)
+
--- backend-api/properties_app/apps.py
+++ backend-api/properties_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PropertiesConfig(AppConfig):
+class PropertiesAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+    name = 'properties_app'
